51f8465d2390f18b44022c9b7f4df5fd8beb95b7
Vitoriono <korovin.vic@gmail.com>
2023-01-02 15:32:01 +0200

    Working with RESTapi

--- weather.js
+++ weather.js
@@ -1,8 +1,22 @@
 #!/usr/bin/env node
 
 import { getArgs } from './helpers/args.js';
-import { printHelp } from './services/log.service.js';
-import { saveKeyValue } from './services/storage.service.js'
+import { getWeather } from './services/api.service.js';
+import { printHelp, printSuccess,printError } from './services/log.service.js';
+import { saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js'
+
+const saveToken = async (token) => {
+  if(!token.length) {
+    printError('Token has not been entered');
+    return;
+  }
+  try {
+    await saveKeyValue(TOKEN_DICTIONARY.token, token);
+    printSuccess('The token has been saved');
+  } catch(e) {
+    printError(e.message);
+  }
+}
 
 const initCLI = () => {
   const args = getArgs(process.argv)
@@ -11,13 +25,14 @@ const initCLI = () => {
   }
   if(args.s){
     
-    
   }
   if(args.t){
-    saveKeyValue('token', args.t);
+    return saveToken(args.t);
   }
   //Output the weather
 
 };
+
+getWeather('kropyvnytskyi');
 
 initCLI();
