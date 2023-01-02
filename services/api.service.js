15ea0521fb96f426ec267a19b108c14eca19ecf5
Vitoriono <korovin.vic@gmail.com>
2023-01-02 18:37:11 +0200

    Error processing

--- services/api.service.js
+++ services/api.service.js
@@ -3,7 +3,7 @@ import https from 'https';
 import { getKeyValue, TOKEN_DICTIONARY } from './storage.service.js';
 
 const getWeather = async (city) => {
-  const token = await getKeyValue (TOKEN_DICTIONARY.token);
+  const token = process.env.TOKEN ?? await getKeyValue (TOKEN_DICTIONARY.token);
   if(!token){
     throw new Error('Token is not exist, enter it via command -t [API_KEY]!');
   }
@@ -21,20 +21,3 @@ const getWeather = async (city) => {
 
 export { getWeather };
 
-// Get response another way then via axios!!! 
-
-//   const url = new URL('https://api.openweathermap.org/data/2.5/weather');
-//   url.searchParams.append('q', city);
-//   url.searchParams.append('appid', token);
-//   url.searchParams.append('lang', 'ru');
-//   url.searchParams.append('units', 'metric');
-
-//   https.get(url, (response) => {
-//     let res = '';
-//     response.on('data', (chunk) => {
-//       res += chunk;
-//     });
-//     response.on('end', () => {
-//       console.log(res);
-//     });
-//   });
--- weather.js
+++ weather.js
@@ -2,7 +2,7 @@
 
 import { getArgs } from './helpers/args.js';
 import { getWeather } from './services/api.service.js';
-import { printHelp, printSuccess,printError } from './services/log.service.js';
+import { printHelp, printSuccess, printError } from './services/log.service.js';
 import { saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js'
 
 const saveToken = async (token) => {
@@ -18,21 +18,39 @@ const saveToken = async (token) => {
   }
 }
 
+const getForcast = async () => {
+  try {
+    const weather = await getWeather(process.env.CITY);
+    console.log(weather); //Pretty output the weather
+  } catch(e) {
+    if(e?.response?.status == 404) {
+      printError('Invalid city');
+    } else if(e?.response?.status == 401) {
+      printError('Invalid token');
+    } else {
+      printError(e.message);
+    }
+  }
+  
+}
+
+
+
 const initCLI = () => {
-  const args = getArgs(process.argv)
+  const args = getArgs(process.argv);
+  
   if(args.h) {
     printHelp();
   }
   if(args.s){
-    
+    // Save city
   }
   if(args.t){
     return saveToken(args.t);
   }
-  //Output the weather
-
+  getForcast();
 };
 
-getWeather('kropyvnytskyi');
+
 
 initCLI();
